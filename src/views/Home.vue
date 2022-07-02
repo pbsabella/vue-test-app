70aec4ad4474e90140db71f233ab381d6cfe3caf
Paula Bianca Abella <pau.abella@gmail.com>
2022-07-02 14:45:08 +0800

    chore: setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
-</template>
-
 <script lang="ts">
-import Vue from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-export default Vue.extend({
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
   name: 'Home',
+
   components: {
     HelloWorld,
   },
 });
 </script>
+
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
